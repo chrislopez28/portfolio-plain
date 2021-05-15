f98afb2dfbafe361cce09ce3d26ca2729feed12d
Chris Lopez <chrislopez28@gmail.com>
2021-05-15 15:23:42 -0700

    Added top drawer menu for small displays

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+particlesJS.load("particles-js", "assets/particles.json", function () {
+  console.log("callback - particles.js config loaded");
+});
+
+function toggleMenu(el) {
+  if (!window.matchMedia("(min-width: 601").matches) {
+    if (el.nodeName !== "LI") {
+      el.classList.toggle("open");
+    } else {
+      document.getElementsByClassName("sandwich")[0].classList.toggle("open");
+    }
+    document.getElementsByClassName("nav-items")[0].classList.toggle("change");
+  }
+}
